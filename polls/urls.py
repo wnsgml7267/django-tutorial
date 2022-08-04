e0edf96be356900eef3bf6599dcf29476f8929c5
wnsgml7267 <dlwns7267@naver.com>
2022-08-05 02:24:33 +0900

    클라이언트와 서버의  req, res를 동작하기 위한 views.py 코드 수정

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    #views.py에 있는 파라미터와 같아야하는 question_id
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
