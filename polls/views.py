6493ec05074fd038a50fad282161ec0d8d5b8a36
wnsgml7267 <dlwns7267@naver.com>
2022-08-05 02:37:00 +0900

    404에러 처리

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -16,7 +16,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    #return HttpResponse("You're looking at question %s." % question_id)
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
+    #return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})    
 def results(request, question_id):
     response = "You're looking at question %s."
     return HttpResponse(response % question_id)
